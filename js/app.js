7c92f1d92351b1eb6012344501e00cf2259796bf
Oleg <oleg@localhost>
2020-12-09 23:45:09 +0300

    33 try

--- js/app.js
+++ js/app.js
@@ -5,9 +5,5 @@
             const coordsEl = document.querySelector('#user-coords');
             coordsEl.textContent = `${position.coords.latitude}, ${position.coords.longitude}`
             coordsEl.parentElement.classList.remove('invisible');
-        });
-    }
-=======
     // TODO: add custom logic here
->>>>>>> 857e3d95477bfbd9c93fb04804a64d0007c1c075
 })();
