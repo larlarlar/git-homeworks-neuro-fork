835839d27996e5e80e3dc574823934be870f12f1
Oleg <oleg@localhost>
2020-12-09 23:38:29 +0300

    comflict solved

--- js/app.js
+++ js/app.js
@@ -6,8 +6,6 @@
             coordsEl.textContent = `${position.coords.latitude}, ${position.coords.longitude}`
             coordsEl.parentElement.classList.remove('invisible');
         });
-    }
-=======
     // TODO: add custom logic here
 >>>>>>> 857e3d95477bfbd9c93fb04804a64d0007c1c075
 })();
